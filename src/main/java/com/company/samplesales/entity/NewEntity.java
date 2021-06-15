be26eb1f1a5b87c57e06c5e97132074b91d958f3
Ilia Morenov <i.morenov@haulmont.com>
2021-06-15 15:31:00 +0400

    add AWS File Storage addon

---
+++ src/main/java/com/company/samplesales/entity/NewEntity.java
@@ -0,0 +1,40 @@
+package com.company.samplesales.entity;
+
+import io.jmix.core.FileRef;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "SALES_NEW_ENTITY")
+@Entity(name = "sales_NewEntity")
+public class NewEntity {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "FILE_")
+    private FileRef file;
+
+    public FileRef getFile() {
+        return file;
+    }
+
+    public void setFile(FileRef file) {
+        this.file = file;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
