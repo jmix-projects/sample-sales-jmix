cba029ef00126ba2ca2089d711ecc8ad17bb53c6
Irina Lovtsova <i.lovtsova@haulmont.com>
2021-07-21 18:20:11 +0400

    add GrapesJS HTML Editor

---
+++ src/main/java/com/company/samplesales/screen/grapesjs/Grapesjs.java
@@ -0,0 +1,10 @@
+package com.company.samplesales.screen.grapesjs;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("sales_Grapesjs")
+@UiDescriptor("GrapesJS.xml")
+public class Grapesjs extends Screen {
+}
